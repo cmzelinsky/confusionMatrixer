18aa83235d82a1cbbc8836247ed41314325dee29
Courtney Zelinsky <cmzelinsky@gmail.com>
2014-11-11 15:51:36 -0500

    Retokenizer Final Draft

--- tokenizeOldXslt.py
+++ tokenizeOldXslt.py
@@ -1,43 +1,50 @@
 from lxml import etree
-import os
-import ntpath
+import os, ntpath, sys
 
-#For doc in docs:
-            
-path = "C:/Users/courtney.zelinsky/Desktop/xslTransformers/"
-doc = "C:\\Users\\courtney.zelinsky\\Desktop\\beta\\temporality_RadiologyNeg\\engine\\6970187.out.out.out.xml"
-parser = etree.XMLParser(encoding="utf-8", recover=True)
-parsedDoc = etree.parse(doc, parser=parser)
-orig = etree.XML(etree.tostring(parsedDoc))
+path = sys.argv[1]           
+#path = "...xslTransformers/"
+#testdoc = "...beta\\temporality_RadiologyNeg\\engine\\..."
 
-#XSLT transformation of parsed doc
-transform = etree.XSLT(etree.parse('C:\\Users\\courtney.zelinsky\\Desktop\\xslTransformers\\tokenizer.xsl'))
-outDoc = transform(parsedDoc)
+if not os.path.exists(path):
+    raise Exception('Invalid path(s)')
 
-#print etree.tostring(outDoc, pretty_print=True)
+if not os.path.exists(path + "\\retokenized"):
+    os.makedirs(path + "\\retokenized")
 
-f = ntpath.basename(doc)[:-4] + ".pyout." + ntpath.basename(doc)[-3:]
-with open(os.path.join(path, f), 'w') as output:
-    output.write(etree.tostring(outDoc))
-output.close()
+docs = filter(lambda x: str(x.split('.')[len(x.split('.'))-1]) == 'xml' , os.listdir(path))
 
-# Compare dom's of each of the original gs and retokenized gs
-origContexts = orig.xpath('//cda:content', namespaces={'cda':'urn:hl7-org:v3'})
-outputContexts = outDoc.xpath('*//content')
+for doc in docs:
+    print doc
+    parser = etree.XMLParser(encoding="utf-8", recover=True)
+    parsedDoc = etree.parse(path + "\\" + doc, parser=parser)
+    orig = etree.XML(etree.tostring(parsedDoc))
 
-for i in range(len(outputContexts)):
-    #think about using attrib somehow?
-    print "orig: ", origContexts[i].xpath('*//cda:.[@ID="entry_' + str(i) + '"]', namespaces={'cda':'urn:hl7-org:v3'})
-    print "output: ", outputContexts[i].xpath('*//.[@ID="entry_' + str(i) + '"]')
-    if origContexts[i].xpath('*//cda:content[@ID="entry_' + str(i) + '"]', namespaces={'cda':'urn:hl7-org:v3'}) != outputContexts[i].xpath('*//content[@ID="entry_' + str(i) + '"]'):
-        print "difference found:"
-        print "orig: ", origContexts[i].xpath('*//cda:content[@ID="entry_' + str(i) + '"]', namespaces={'cda':'urn:hl7-org:v3'})
-        print "output: ", outputContexts[i].xpath('*//content[@ID="entry_' + str(i) + '"]')
-        #print etree.tostring(elem), etree.tostring(retokElem)
-            # 1 - collect elem's ID attribute in a var
-            # 2 - change elem's ID attrribute to be retokElem's entry number
-            # 3 - if there are any ref attributes of narrativeBinding that are equal to the elem ID attribute value,
-            #     change it to be the retokElem's ID attribute value
-    else:
-        print "match, continuing"
-        continue
+    #XSLT transformation of parsed doc
+    transform = etree.XSLT(etree.parse('C:\\Users\\courtney.zelinsky\\Desktop\\xslTransformers\\tokenizer.xsl'))
+    outDoc = transform(parsedDoc)
+
+    # Compare dom's of each of the original gs and retokenized gs
+    origContexts = orig.xpath('//cda:content', namespaces={'cda':'urn:hl7-org:v3'})
+    outputContexts = outDoc.xpath('*//content')
+
+    narrBindings = orig.xpath('*//mm:narrativeBinding', namespaces={'mm':'http://mmodal.com/cdaExtensions'})
+
+    for i in range(len(origContexts)):
+        origContext = etree.XML(etree.tostring(origContexts[i]))
+        outputContext = etree.XML(etree.tostring(outputContexts[i]))
+        #print etree.tostring(outputContext) #up to 228
+        if origContext.attrib['ID'] != outputContext.attrib['ID']:
+            #if there's a narrative binding with an ID attribute matching the original document's ID attribute
+            if outDoc.find('*//mm:narrativeBinding[@ref="' + origContext.attrib['ID'] +'"]/*[1]', namespaces={"mm":"http://mmodal.com/cdaExtensions"}) is not None:
+                # replace the ID attribute of the original narrative binding with the ID attribute of the output, so as to coordinate MIM bindings with the new tokenization
+                outDoc.find('*//mm:narrativeBinding[@ref="' + origContext.attrib['ID'] +'"]', namespaces={"mm":"http://mmodal.com/cdaExtensions"}).attrib['ref'] = outputContext.attrib['ID']
+                continue
+            else:
+                continue
+
+    f = ntpath.basename(doc)[:-4] + ".pyout." + ntpath.basename(doc)[-3:]
+    with open(os.path.join(path + "\\retokenized", f), 'w') as output:
+        output.write(etree.tostring(outDoc))
+    output.close()
+
+print("Retokenization complete! " + str(len(docs)) + " file(s) written to " + path + "\\retokenized")
